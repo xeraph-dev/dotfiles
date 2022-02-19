7db2d1594b2f9e441d2b018d7bb896e1714d1016
Adrian <adrianrlc99@gmail.com>
2022-02-19 17:15:46 -0500

    update

--- nixos/programs.nix
+++ nixos/programs.nix
@@ -3,7 +3,6 @@
 {
   programs = {
     adb.enable = true;
-    appgate-sdp.enable = true;
     seahorse.enable = true;
     neovim = (import ./programs/neovim.nix pkgs);
     zsh = (import ./programs/zsh.nix pkgs);
